e8de3145f531dd99a46c4fea0e2f21ce861368ef
lukemorse <luke.morse@gmail.com>
2020-09-18 14:22:25 -0500

    Add additional properties to Post model

--- HCA Test App/Model/Post.swift
+++ HCA Test App/Model/Post.swift
@@ -9,25 +9,31 @@
 import Foundation
 
 struct Post: Encodable {
-    var answerCount: Int
-    
-    init(answer_count: Int) {
-        self.answerCount = answer_count
-    }
+    let answerCount: Int
+    let displayName: String
+    let title: String
+    let score: Int
 
     private enum CodingKeys: String, CodingKey {
-        case answer_count = "answer_count"
+        case answerCount = "answer_count"
+        case displayName = "display_name"
+        case title = "title"
+        case score = "score"
+        
     }
 
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(answerCount, forKey: .answer_count)
+        try container.encode(answerCount, forKey: .answerCount)
     }
 }
 
 extension Post: Decodable {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        answerCount = try container.decode(Int.self, forKey: .answer_count)
+        answerCount = try container.decode(Int.self, forKey: .answerCount)
+        displayName = try container.decode(String.self, forKey: .displayName)
+        title = try container.decode(String.self, forKey: .title)
+        score = try container.decode(Int.self, forKey: .score)
     }
 }
